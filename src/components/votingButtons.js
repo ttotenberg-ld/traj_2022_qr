6e6b0e8e242b50a7bed3a4af1bd32c71f7776179
Tom Totenberg <ttotenberg@launchdarkly.com>
2022-11-09 09:25:15 -0600

    Added responsive buttons

--- src/components/votingButtons.js
+++ src/components/votingButtons.js
@@ -12,8 +12,48 @@ import React, { useState } from 'react';
 
 
 const VotingButtons = ({ flags, ldClient /*, ...otherProps */ }) => {
+  // Evaluate LaunchDarkly flags.
+  // 'votingButtons' determines whether buttons are shown
+  // 'buttonColor' controls the color of the bottom button
   let showButtons = ldClient.variation("votingButtons");
   let buttonColor = ldClient.variation("buttonColor");
+
+  const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 10px;
+  cursor: pointer;
+
+  &:disabled {
+    color: grey;
+    opacity: 0.7;
+    cursor: default;
+  }
+  `;
+
+  const ColoredButton = styled.button`
+  background-color: ${buttonColor};
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 10px;
+  cursor: pointer;
+
+  &:disabled {
+    color: grey;
+    opacity: 0.7;
+    cursor: default;
+  }
+  `;
+
+  const ButtonGroup = styled.div`
+  display: grid;
+  `;
+
 
   const sounds = [
     pop1,
@@ -27,34 +67,15 @@ const VotingButtons = ({ flags, ldClient /*, ...otherProps */ }) => {
   ];
 
   const [randomSound, setRandomSound] = useState(sounds[Math.floor(Math.random()*sounds.length)]);
+  const [disable, setDisable] = useState(false);
+  const [headerText, setHeaderText] = useState('Favorite Session?');
 
-  const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 10px;
-  cursor: pointer;
-  `;
-
-  const ColoredButton = styled.button`
-  background-color: ${buttonColor};
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 10px;
-  cursor: pointer;
-  `;
-
-  const ButtonGroup = styled.div`
-  display: grid;
-  `;
 
   let [play] = useSound(randomSound);
 
   function clicked(metric) {
+    setDisable(true);
+    setHeaderText('Thanks for voting!');
     setRandomSound(sounds[Math.floor(Math.random()*sounds.length)]);
     play();
     ldClient.track(metric);
@@ -63,13 +84,13 @@ const VotingButtons = ({ flags, ldClient /*, ...otherProps */ }) => {
   return showButtons ? (
     <div>
       <br />
-      <h1 style={{ color: 'black' }}><center>Favorite Session?</center></h1>
+      <h1 style={{ color: 'black' }}><center>{headerText}</center></h1>
       <ButtonGroup>
-        <Button onClick={() => {clicked('button-1')}}>
+        <Button disabled={disable} onClick={() => {clicked('button-1')}}>
           <b>Tom</b>
         </Button>
         
-        <ColoredButton onClick={() => {clicked('button-2')}}>
+        <ColoredButton disabled={disable} onClick={() => {clicked('button-2')}}>
           <b>Tom but in {buttonColor}</b>
         </ColoredButton>
       </ButtonGroup>
